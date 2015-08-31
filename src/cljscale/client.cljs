fd12c17d719cc009e7b8a9b64b51e62a152ecddd
marius herrin <mariusherring@gmail.com>
2015-08-31 21:24:42 +0200

    started on script

--- src/cljscale/client.cljs
+++ src/cljscale/client.cljs
@@ -1,7 +1,27 @@
 (ns cljscale.client
   (:require [quiescent.core :as q]
-            [quiescent.dom :as d]))
-
+            [quiescent.dom :as d]
+            [cljscale.theory :as t]))
 
 (enable-console-print!)
-(println "Hello world")
+
+(def fretboard (atom [[{:note "E"}{:note "F"}{:note "F#"}{:note "G"}{:note "G#"}]
+                      [{:note "E"}{:note "F"}{:note "F#"}{:note "G"}{:note "G#"}]]))
+
+(declare FretBoard)
+
+(defn render []
+  (q/render (FretBoard @fretboard)
+            (.getElementById js/document "main")))
+
+(q/defcomponent Fret [fret]
+  (println (t/test))
+  (apply d/div {:className "fret"} (:note fret)))
+
+(q/defcomponent String [frets]
+  (apply d/div {:className "string"} (mapv Fret frets)))
+
+(q/defcomponent FretBoard [fretboard]
+  (apply d/div {:className "fretboard"} (mapv String fretboard)))
+
+(render)
---
+++ src/cljscale/theory.cljs
@@ -0,0 +1,8 @@
+(ns cljscale.theory)
+
+(def note-map {"E" "F"
+               "F" "F#"})
+
+(defn map-note [note])
+
+(defn test [] "Hello")
